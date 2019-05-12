8a8f836ea13f8cee291c435e5491fdfc8a2bbcb7
Jordan Gillard <j.gillard@gastrofix.com>
2019-05-12 09:33:02 +0200

    Solved max toys alg.

--- mark_and_toys/max_toys.py
+++ mark_and_toys/max_toys.py
@@ -1,6 +1,12 @@
 """Taken from www.hackerrank.com/challenges/mark-and-toys/"""
 
 
-def max_toys(toy_prices, k) -> int:
+def max_toys(prices, k) -> int:
     """Returns an int representing the max number of toys that can be bought with k."""
-    pass
+    prices.sort()
+    toys = 0
+    for toy_price in prices:
+        if k - toy_price >= 0:
+            toys += 1
+            k -= toy_price
+    return toys
--- mark_and_toys/test.py
+++ mark_and_toys/test.py
@@ -4,7 +4,8 @@ from mark_and_toys.max_toys import max_toys
 
 
 @pytest.mark.parametrize("toy_prices, budget, expected", [
-    ([1, 2, 3, 4], 7, 3)
+    ([1, 2, 3, 4], 7, 3),
+    ([1, 12, 5, 111, 200, 1000, 10], 50, 4)
 ])
 def test_max_toys(toy_prices, budget, expected):
     assert max_toys(toy_prices, budget) == expected

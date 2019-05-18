55d71f17aae640420ace1b6de739015635c43daf
Jordan Gillard <j.gillard@gastrofix.com>
2019-05-18 17:29:48 +0100

    Added test case stuff for leaderboard alg

--- medium/climbing_leaderboard/algorithm.py
+++ medium/climbing_leaderboard/algorithm.py
@@ -3,6 +3,14 @@ from typing import List, Set, Optional
 
 
 def climbing_leaderboard(leaderboard_scores: List[int], alices_scores: List[int]) -> List[int]:
+    """Steps:
+        Iterate over each score, place -= 1 for each score
+        If Alice has no more scores, then return Alice's places.
+        If Alice's score is more than the score, move onto the next score.
+        If Alice's score is less than the score, store Alice's place
+        If Alice's score is equal to the score, increment the place by one and store it.
+        Repeat for each of Alice's scores.
+    """
     leaderboard_scores: Set[int] = set(leaderboard_scores)
     leaderboard_scores: List[int] = sorted(list(leaderboard_scores), reverse=False)
     place: int = len(leaderboard_scores) + 1
@@ -24,10 +32,3 @@ def climbing_leaderboard(leaderboard_scores: List[int], alices_scores: List[int]
             index += 1
         place -= 1
     return alices_places
-
-# Iterate over each score, place -= 1 for each score
-# If Alice has no more scores, then return Alice's places.
-# If Alice's score is more than the score, move onto the next score.
-# If Alice's score is less than the score, store Alice's place
-# If Alice's score is equal to the score, increment the place by one and store it.
-# Repeat for each of Alice's scores.

9548d035287397d3b4144904d512d317e83231ce
Jordan Gillard <j.gillard@gastrofix.com>
2019-06-23 17:32:34 +0200

    refactored encryption alg to be more efficient.

--- hacker_rank_problems/medium/encryption/alg.py
+++ hacker_rank_problems/medium/encryption/alg.py
@@ -5,17 +5,10 @@ def encryption(sentence: str) -> str:
     sqrt_len: float = math.sqrt(len(sentence))
     num_col: int = math.ceil(sqrt_len)
     s_no_space: str = sentence.replace(' ', '')
-    rows: list = []
+    rows: list = [""] * num_col
     for i in range(num_col):
         i = i * num_col
         new_row = s_no_space[i: i+num_col]
-        rows.append(new_row)
-    encrypted_sentence: str = ''
-    for i in range(num_col):
-        for word in rows:
-            try:
-                encrypted_sentence += word[i]
-            except IndexError:
-                continue
-        encrypted_sentence += ' '
-    return encrypted_sentence.rstrip()
+        for i in range(len(new_row)):
+            rows[i] += new_row[i]
+    return ' '.join(rows)

2a73b333b22d373d89044fe99b44de1d24dca7a6
Jordan Gillard <j.gillard@gastrofix.com>
2019-06-25 18:04:14 +0200

    much more elegant solution to flipping_bits

--- hacker_rank_problems/easy/flipping_bits/alg.py
+++ hacker_rank_problems/easy/flipping_bits/alg.py
@@ -5,8 +5,5 @@ def flipping_bits(num: int) -> int:
     """You will be given a list of 32 bit unsigned integers. Flip all the bits and print the result as an
     unsigned integer.
     """
-    byte_utf: str = '{:032b}'.format(num)
-    flipped: str = ''
-    for str_num in byte_utf:
-        flipped += str(int(not int(str_num)))
-    return int(flipped, 2)
+    flipper = 4294967295
+    return num ^ flipper
